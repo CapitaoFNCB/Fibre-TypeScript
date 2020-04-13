0a10b5a4b59f98de08528a5d883c2fe0eb3f3883
root <root@vmi371602.contaboserver.net>
2020-04-13 11:29:56 +0200

    test

--- src/utils/Config.ts
+++
@@ -1,4 +0,0 @@
-export let token: string = "NjkzMTE4OTgxNzIwMTEzMTgy.XpQrmQ.x9-hd7isgsPmqnQ9FMKokLcelMY";
-export let owners: string | string[] = ["665237546183294999"];
-export let nodes: any = [{host: "localhost", port: 2333, password: "youshallnotpass"}]
-export let connection: any = "mongodb+srv://Fibre:Waterside321!@cluster0-6jp3i.mongodb.net/test?retryWrites=true&w=majority" 
